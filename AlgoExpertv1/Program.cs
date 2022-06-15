b320122f81cdc63d57aee6e787045a0dab95b35c
Matthew Miller <mdm6312@psu.edu>
2022-06-15 10:02:38 -0400

    Update TournamentWinner problem with comments and starter code

--- AlgoExpertv1/Program.cs
+++ AlgoExpertv1/Program.cs
@@ -8,12 +8,45 @@ namespace AlgoExpertv1
         {
             int[] array = new int[] { 1,2,3,5,6,8,9 };
 
-            Console.WriteLine();
-
-            foreach (int val in SortedSquaredArray(array))
+            List<List<string>> competitions = new List<List<string>>
             {
-                Console.Write(val + " ");
-            }
+                new List<string>{ "HTML", "C#" },
+                new List<string>{ "C#", "Python" },
+                new List<string>{ "Python", "HTML" }
+            };
+
+            List<int> results = new List<int> { 0, 0, 1 };
+
+            Console.WriteLine(TournamentWinner(competitions, results));
+        }
+
+        /// <summary>
+        /// Algo tournament taking place in which teams of programmers compete against each other to solve 
+        /// algorithmic problems as fast as possible. Teams compete in a round robin, where each team
+        /// faces off against all other teams. Only two teams compete against each other at a time, and for
+        /// each competition, one team is designated the home team, while the other team is the away team. In
+        /// each competition there's always one winner and one loser; there are no ties. A team receives 3 points
+        /// if it wins and 0 points if it loses. The winner of the tournament is the team that receives the most amount
+        /// of points.
+        /// 
+        /// Given an array of pairs representing the teams that have competed against each other and an array containing
+        /// the results of each competition, write a function that returns the winner of the touranment. The input arrays
+        /// are named competitions and results, respectively. The competitions array has alements inn the form of 
+        /// [homeTeam, awayTeam]. where each team is a string of at most 30 characters representing the name of the team.
+        /// The results array contains information about the winner of the competitions[i], where a 1 in the results array
+        /// means that the home team in the corresponding competition won and a 0 means that the away team won.
+        /// 
+        /// It's guarenteed that exactly one team will win the tournament and that each team will compete against
+        /// all other teams exactly one. It's also guarenteed that the tournament will always have at least two teams.
+        /// </summary>
+        /// <param name="competitions"></param>
+        /// <param name="results"></param>
+        /// <returns></returns>
+        public static string TournamentWinner(List<List<string>> competitions,
+            List<int> results)
+        {
+            // Write your code here.
+            return "";
         }
 
         /// <summary>

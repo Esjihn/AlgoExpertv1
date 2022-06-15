ed2be4061bfac564472c24a0678b48f531dea3a6
Matthew Miller <mdm6312@psu.edu>
2022-06-15 09:36:19 -0400

    Solution for ValidateSubSequence

--- AlgoExpertv1/Program.cs
+++ AlgoExpertv1/Program.cs
@@ -6,7 +6,44 @@ namespace AlgoExpertv1
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> array = new List<int> { 5, 1, 22, 25, 6, -1, 8, 10 };
+            List<int> sequence = new List<int> { 1, 6, -1, 10 };
+
+            Console.WriteLine(ValidateSubsequence(array, sequence));
+        }
+
+        public static bool ValidateSubsequence(List<int> array,
+            List<int> sequence)
+        {
+            //for (int i = 0; i < array.Count; i++)
+            //{
+            //    for (int j = 0; j < sequence.Count; j++)
+            //    {
+            //        if (sequence[j] == array[i])
+            //        {
+            //            sequence.RemoveAt(j);
+            //            break;
+            //        }
+            //    }
+            //}
+
+            //return !sequence.Any();
+
+            int seqIndex = 0;
+            foreach (var val in array)
+            {
+                if (seqIndex == sequence.Count)
+                {
+                    break;
+                }
+
+                if (sequence[seqIndex] == val)
+                {
+                    seqIndex++;
+                }
+            }
+
+            return seqIndex == sequence.Count;
         }
     }
 }
